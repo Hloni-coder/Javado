b739c61ec887c16a680e5135e5b7aa2192d34cd6
Lehlohonolo <lemohaljhb024@student.wethinkcode.co.za>
2025-02-07 08:38:13 +0200

    Task complete

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,32 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
-
-public class TaskManager {
-    private List<String> tasks; // hint: will change in iteration 3
+class TaskManager {
+    private List<String> tasks;
 
     public TaskManager() {
-        // Initialize tasks list
         tasks = new ArrayList<>();
-
     }
 
     public void addTask(String task) {
         tasks.add(task);
+        System.out.println("Task added: " + task);
     }
 
     public List<String> listTasks() {
         return tasks;
-
     }
 
-
-    public void deleteTask(String task){
-//        leave for iteration 4
+    public void deleteTask(String task) {
+        if (tasks.remove(task)) {
+            System.out.println("Task deleted: " + task);
+        } else {
+            System.out.println("Task not found: " + task);
+        }
     }
 
     public void exit() {
-        // leave for iteration 2
+       
     }
 }
